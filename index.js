a4f4f793e6dd506198ca27ce1154098d390730e0
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-10-11 23:34:50 +0000

    update with solutions

--- index.js
+++ index.js
@@ -9,7 +9,41 @@
 // Input: nums = [-1,0,3,5,9,12], target = 9
 // Output: 4
 // Explanation: 9 exists in nums and its index is 4
-
+//binary search cuts your list in half to find what you are looking for
+nums = [-1, 0, 3, 5, 9, 12]
+target = 9
 var search = function(nums, target) {
-  
+
+////104ms runtime////
+    let low = 0,
+    high = nums.length - 1;
+
+      while (low <= high) {
+        const mid = Math.floor((low + high) / 2);
+
+        if (nums[mid] < target) {
+          low = mid + 1;
+        } else if (nums[mid] > target) {
+          high = mid - 1;
+        } else return mid;
+      }
+      return -1;
+
+////105 ms runtime////
+//     let lowestTarget = 0,
+//     highestTarget = nums.length - 1;
+
+//     while (lowestTarget <= highestTarget) {
+//         // let medTarget = lowestTarget + ((highestTarget - lowestTarget)/2);
+//         let medTarget =  Math.floor((highestTarget + lowestTarget)/2);
+//         if (nums[medTarget] < target) {
+//              lowestTarget = medTarget + 1;
+//         } else if (nums[medTarget] > target) {
+//              highestTarget = medTarget - 1; 
+//         } else 
+//             return medTarget
+            
+//     }   
+//         return -1;
+
 }
